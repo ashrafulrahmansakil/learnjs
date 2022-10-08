45fa4649d3238f4beda341d8060b60013b27d680
ashraful rahman sakil <sakil55183@gmail.com>
2022-10-08 23:17:00 +0600

    learnjs

--- js/server.js
+++ js/server.js
@@ -4,17 +4,18 @@
 // console.log(os.homedir());
 
 /*
-    Create new http Server anisul islam
+    Create new http Server
 */
 
 import { createServer } from "http";
-const port = 3000;
+const PORT = 3000;
 const hostname = "127.0.0.1";
 const server = createServer((req, res) => {
-  res.end('<h1> hi i am server </h1>');
-  //res.end('hi i am server');
+  res.writeHead(200, { "content-Type": "index.html" });
+  res.write("<h1> hi i am server </h1>");
+  res.end();
 });
 
-server.listen(port, hostname, () => {
-  console.log(`http://${hostname}:${port} successfully`);
+server.listen(PORT, hostname, () => {
+  console.log(`server successfully http://${hostname}:${PORT}`);
 });
