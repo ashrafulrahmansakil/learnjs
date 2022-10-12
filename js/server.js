5bcae1ee24edaf0074b4bda139c3ac0370194c10
ashraful rahman sakil <sakil55183@gmail.com>
2022-10-12 20:01:56 +0600

    learnjs

--- js/server.js
+++ js/server.js
@@ -1,19 +1,28 @@
-// nodejs elements method
-// import os from 'os';
-//console.log(os.userInfo());
-// console.log(os.homedir());
-
 /*
     Create new http Server
 */
-
 import { createServer } from "http";
 const PORT = 3000;
 const hostname = "127.0.0.1";
+import fs from "fs";
+const handelReadFile = (fileName, statuscode, req, res) => {
+  fs.readFile(fileName, "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    }
+  });
+};
 const server = createServer((req, res) => {
-  res.writeHead(200, { "content-Type": "index.html" });
-  res.write("<h1> hi i am server </h1>");
-  res.end();
+  if (req.url == "/") {
+    handelReadFile("js/server.js", 200, req, res);
+  }
+  else if (req.url == "/js/index.html") {
+    handelReadFile("js/index.html", 200, req, res);
+  }
 });
 
 server.listen(PORT, hostname, () => {
